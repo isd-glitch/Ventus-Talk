b3856ce85d1a414315f7c8ab450e91402e554e3a
石田　尚幹 <133995608+Stasshe@users.noreply.github.com>
2025-01-14 10:30:33 +0900

    Create firebase_keys.js

---
+++ public/firebase_keys.js
@@ -0,0 +1,59 @@
+// firebase_keys.js
+
+const firebaseConfigDev = {
+  apiKey: "AIzaSyBtqr92DkuyzQZjCF59P-ksJiuY6h-sYw0",
+  authDomain: "ventus-talk-dev.firebaseapp.com",
+  projectId: "ventus-talk-dev",
+  storageBucket: "ventus-talk-dev.firebasestorage.app",
+  messagingSenderId: "222377294941",
+  appId: "1:222377294941:web:9031aca4bbdad47b5e61b4",
+  measurementId: "G-SHZ2Y05NS3"
+};
+
+
+const firebaseConfig1 = {
+    apiKey: "YOUR_API_KEY_1",
+    authDomain: "YOUR_AUTH_DOMAIN_1",
+    projectId: "Ventus-Talk-1",
+    storageBucket: "YOUR_STORAGE_BUCKET_1",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_1",
+    appId: "YOUR_APP_ID_1"
+};
+
+const firebaseConfig2 = {
+    apiKey: "YOUR_API_KEY_2",
+    authDomain: "YOUR_AUTH_DOMAIN_2",
+    projectId: "Ventus-Talk-2",
+    storageBucket: "YOUR_STORAGE_BUCKET_2",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_2",
+    appId: "YOUR_APP_ID_2"
+};
+
+// 他のFirebase設定も同様に追加します...
+
+const firebaseConfigUsers = {
+    apiKey: "YOUR_API_KEY_USERS",
+    authDomain: "YOUR_AUTH_DOMAIN_USERS",
+    projectId: "Ventus-Talk-Users",
+    storageBucket: "YOUR_STORAGE_BUCKET_USERS",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_USERS",
+    appId: "YOUR_APP_ID_USERS"
+};
+
+const firebaseConfigInfo = {
+    apiKey: "YOUR_API_KEY_INFO",
+    authDomain: "YOUR_AUTH_DOMAIN_INFO",
+    projectId: "Ventus-Talk-info",
+    storageBucket: "YOUR_STORAGE_BUCKET_INFO",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_INFO",
+    appId: "YOUR_APP_ID_INFO"
+};
+
+export {
+firebaseConfigDev,
+    firebaseConfig1,
+    firebaseConfig2,
+    // 他のエクスポートも追加します...
+    firebaseConfigUsers,
+    firebaseConfigInfo
+};
